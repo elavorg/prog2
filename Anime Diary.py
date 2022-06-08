2a3bc1b33a52116dacd8cb39508dc5a9471ecd81
elavorg <91332736+elavorg@users.noreply.github.com>
2022-06-08 22:15:41 +0200

    html mit code verknüpft

--- Anime Diary.py
+++ Anime Diary.py
@@ -1,12 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask("Hello World")
 
+@app.route("/hinzufügen")
+def watchlist_empfehlung():
+    return render_template('Hinzufügen.html', name="Anime Diary")
 
-@app.route('/hello')
-def hello_world():
-    return 'Hello, World!'
+
+@app.route("/hinzufügen", methods=['GET', 'POST'])
+def erfassung_ausgeben():
+    if request.method == 'GET':
+        return render_template('Hinzufügen.html')
+    if request.method == 'POST':
+        erfasste_eingabe = request.form['Name Anime']
+        rueckgabe_string = "Eingaben für " + erfasste_eingabe + " werden gespeichert und der Filmauswahl hinzugefügt!"
+        return rueckgabe_string
+
+
+@app.route("/auswahl")
+def watchlist_vorschlag():
+    return render_template('Auswahl.html', vorschlag="Anime auswählen")
+
+
+@app.route("/test")
+def test():
+    return "passt!"
 
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
+
